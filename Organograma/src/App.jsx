5662428fa8006ebb53f0f129e84b415f41c82383
Davi Macedo <134751279+DaviMacedo07@users.noreply.github.com>
2025-01-29 14:39:03 -0300

    ----

--- Organograma/src/App.jsx
+++ Organograma/src/App.jsx
@@ -1,14 +1,26 @@
+import { useState } from 'react'
 import Banner from './componentes/Banner/Banner'
 import Formulario from './componentes/Formulario'
+import Time from './componentes/Time';
 
 function App() {
+  const [colaboradores, setColaboradores] = useState([]);
+
+  const aoNovoColaboradorAdd = (colaborador) => {
+    console.log(colaborador);  // Aqui você pode verificar o que está sendo passado
+    setColaboradores([...colaboradores, colaborador]);  // Atualiza a lista de colaboradores
+  };
 
   return (
-    <div className='App'>
-    <Banner></Banner> 
-    <Formulario></Formulario>
-    </div>
-  )
-}
+    <div className="App">
+      <Banner />
+      <Formulario aoColaboradorCadastrado={aoNovoColaboradorAdd} />
+      <Time nome="Programação"></Time>
+      <Time nome="Frontend"></Time>
+      <Time nome="Data Science"></Time>
 
+
+    </div>
+  );
+}
 export default App
--- Organograma/src/componentes/Formulario/index.jsx
+++ Organograma/src/componentes/Formulario/index.jsx
@@ -2,8 +2,8 @@ import "./Formulario.css";
 import CampoTexto from "../CampoTexto";
 import ListaSuspensa from "../ListaSuspensa";
 import Botao from "../Botao/Index";
-const Formulario = () => {
-
+import { useState } from "react";
+const Formulario = (props) => {
   const times = [
     "Programação",
     "Front-End",
@@ -11,24 +11,55 @@ const Formulario = () => {
     "Back-End",
     "DevOps",
     "Ux e Design",
-    "Mobile"
-  ]
+    "Mobile",
+  ];
+
+  const [nome, setNome] = useState("")
+  const [cargo, setCargo] = useState("")
+  const [imagem, setImagem] = useState("")
+  const [time, setTime] = useState("")
 
   const aoSalvar = (evento) => {
-    evento.preventDefault()
-    console.log("O formulário foi submetido")
-  }
+    evento.preventDefault();
+    props.aoColaboradorCadastrado({ nome, cargo, imagem, time });
+  };
   return (
     <section className="formulario">
       <form onSubmit={aoSalvar}>
         <h2>Preencha os dados para criar o card do colaborador</h2>
-        <CampoTexto label="Nome" obrigatorio={true} placeholder="Digite seu nome"></CampoTexto>
-        <CampoTexto label="Cargo" obrigatorio={true} placeholder="Digite seu cargo"></CampoTexto>
-        <CampoTexto label="Imagem" obrigatorio={true} placeholder="Digite o endereço da imagem"></CampoTexto>
-        <ListaSuspensa label="Time" itens={times}></ListaSuspensa>
-        <Botao >
-          Criar Card
-        </Botao>
+        <CampoTexto
+          label="Nome"
+          obrigatorio={true}
+          placeholder="Digite seu nome"
+          valor={nome}
+          aoAlterado={valor => setNome(valor)}
+        ></CampoTexto>
+        <CampoTexto
+          label="Cargo"
+          obrigatorio={true}
+          placeholder="Digite seu cargo"
+          valor={cargo}
+          aoAlterado={valor => setCargo(valor)}
+
+        ></CampoTexto>
+        <CampoTexto
+          label="Imagem"
+          obrigatorio={true}
+          placeholder="Digite o endereço da imagem"
+          valor={imagem}
+          aoAlterado={valor => setImagem(valor)}
+
+        ></CampoTexto>
+        <ListaSuspensa 
+        label="Time" 
+        itens={times} 
+        obrigatorio={true}
+        valor = {time}
+        aoAlterado={valor => setTime(valor)}
+        >
+
+        </ListaSuspensa>
+        <Botao>Criar Card</Botao>
       </form>
     </section>
   );
